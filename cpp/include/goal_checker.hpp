040c13824d73e2efdb77d44a254baa157a628cbf
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2023-11-26 11:54:44 -0500

    updated custom goal state checker

--- cpp/include/goal_checker.hpp
+++ cpp/include/goal_checker.hpp
@@ -1,5 +1,14 @@
 #pragma once
 
+#include <pinocchio/fwd.hpp>
+#include <pinocchio/algorithm/frames.hpp>
+#include <pinocchio/algorithm/jacobian.hpp>
+#include <pinocchio/algorithm/joint-configuration.hpp>
+#include <pinocchio/algorithm/kinematics.hpp>
+#include <pinocchio/algorithm/model.hpp>
+#include <pinocchio/multibody/model.hpp>
+#include <pinocchio/parsers/urdf.hpp>
+
 #include <ompl/base/SpaceInformation.h>
 #include <ompl/base/goals/GoalRegion.h>
 #include <ompl/base/spaces/RealVectorStateSpace.h>
@@ -13,4 +22,10 @@ public:
 private:
   mutable double sum;
   mutable double error;
+
+  pinocchio::Model model;
+  mutable pinocchio::Data data;
+  std::string urdf_filename;
+
+  pinocchio::Model::FrameIndex boxId;
 };
--- cpp/src/goal_checker.cpp
+++ cpp/src/goal_checker.cpp
@@ -3,6 +3,14 @@
 CustomGoal::CustomGoal(const ompl::base::SpaceInformationPtr& si) : ompl::base::GoalRegion(si)
 {
   setThreshold(0.3);
+
+  // build pin_robot from urdf
+  urdf_filename = "/home/AnywareInterview/python/robots/robot.urdf";
+  pinocchio::urdf::buildModel(urdf_filename, model);
+  data = pinocchio::Data(model);
+
+  // get link frame indices
+  boxId = model.getFrameId("box");
 }
 
 double CustomGoal::distanceGoal(const ompl::base::State* state) const
@@ -15,7 +23,20 @@ double CustomGoal::distanceGoal(const ompl::base::State* state) const
   double q2 = realState->values[1];
   double q3 = realState->values[2];
 
-  sum = std::pow(q1 - 0.0, 2) + std::pow(q2 + M_PI / 2, 2) + std::pow(q3 - 0.0, 2);
+  // set the joint angle and joint velocity vectors
+  Eigen::Matrix<double, 3, 1> q;
+  q << q1, q2, q3;
+
+  Eigen::Matrix<double, 3, 1> dq;
+  dq << 0.0, 0.0, 0.0;
+
+  // update pinocchio robot model
+  pinocchio::forwardKinematics(model, data, q, dq);
+  pinocchio::updateFramePlacements(model, data);
+
+  auto p_box = data.oMf[boxId].translation();
+
+  sum = std::pow(p_box(0) - 0.5, 2) + std::pow(p_box(1) + 1.0, 2) + std::pow(p_box(2) - 0.05, 2);
   error = std::sqrt(sum);
 
   return error;
